0ebe8ef5a50893534922e47845cc6315dce6449f
vuongvv <vuongvv@hblab.vn>
2022-12-26 16:19:13 +0700

    doc api

--- controller/user.go
+++ controller/user.go
@@ -29,8 +29,8 @@ func (c *UserController) GetUsers(ctx *gin.Context) {
 }
 
 // GetUser godoc
-// @Summary      Create an user
-// @Description  Create an user
+// @Summary      Get user
+// @Description  Get user
 // @Tags         user
 // @Accept       json
 // @Produce      json
@@ -39,11 +39,23 @@ func (c *UserController) GetUsers(ctx *gin.Context) {
 // @Failure      400  {object}  domain.ErrorResponse
 // @Failure      404  {object}  domain.ErrorResponse
 // @Failure      500  {object}  domain.ErrorResponse
-// @Router       /users/{id} [get]
+// @Router       /api/v1/users/{id} [get]
 func (c *UserController) GetUser(ctx *gin.Context) {
 
 }
 
+// CreateUser godoc
+// @Summary      Create an user
+// @Description  Create an user
+// @Tags         user
+// @Accept       json
+// @Produce      json
+// @Param        body  body object  true  "Body"
+// @Created      201  {object}  domain.User
+// @Failure      400  {object}  domain.ErrorResponse
+// @Failure      404  {object}  domain.ErrorResponse
+// @Failure      500  {object}  domain.ErrorResponse
+// @Router       /api/v1/users [post]
 func (c *UserController) CreateUser(ctx *gin.Context) {
 	user := domain.CreateUserReq{}
 	if !bindJSON(ctx, &user) {
--- main.go
+++ main.go
@@ -23,9 +23,9 @@ import (
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
 // @host      localhost:8080
-// @BasePath  /api/v1
-
-// @securityDefinitions.basic  BasicAuth
+// @securityDefinitions.apikey Bearer
+// @in header
+// @name Authorization
 func main() {
 	fmt.Println("---- Hello world! ----")
 
